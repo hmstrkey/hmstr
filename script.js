cadece64f1197d1fc66b15a89a77338eca8fb6f8
hmstrkey <akkaynt123akk1@gmail.com>
2024-08-08 02:26:22 +0600

    Update script.js

--- script.js
+++ script.js
@@ -547,7 +547,7 @@ function startCountdown(targetDate) {
     const interval = setInterval(updateCountdown, 1000);
 }
 
-const targetDate = new Date('Sun Aug 07 2024 20:30:00 GMT+0000').getTime();
+const targetDate = new Date('Sun Aug 08 2024 20:30:00 GMT+0000').getTime();
 const beforeFourHours = new Date(targetDate - 4 * 60 * 60 * 1000).getTime();
 const nowTime = new Date().getTime();
 
@@ -556,7 +556,7 @@ if (targetDate > nowTime && beforeFourHours < nowTime) {
 }
 
 const dateParam = getQueryParam('date');
-const date = '2024-08-07';
+const date = '2024-08-08';
 
 const dateDisplay = document.getElementById('date-display');
 dateDisplay.textContent = date;
